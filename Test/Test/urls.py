81ecac5239583f5474f26e7de51c19f3c75d01e4
Priyanshu Tripathi <priyanshu.tripathi@volkswagen.co.in>
2024-08-17 23:27:17 +0530

    Adding Html

--- Test/Test/urls.py
+++ Test/Test/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
 urlpatterns = [
+    path('' , home , name="home"),
+    path('contact/' , contact , name="contact"),
+    path('about/' , about , name="about"),
     path('admin/', admin.site.urls),
+
 ]
--- Test/home/views.py
+++ Test/home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+   peoples = [
+      {'name':'Priyanshu','age':25,},
+      {'name':'Himashu','age':26, },
+      {'name':'Golu','age':10,}
+   ]
+   return render(request , "index.html", context = {'peoples':peoples})
+
+def about(request):
+   return render (request,"about.html")
+
+def contact(request):
+   context = {'page': 'about'}
+   return render (request,"contact.html",context)
