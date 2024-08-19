6b5b62b6a8fb6ac7a1a4fea01bc6634ae207b0a2
Priyanshu Tripathi <priyanshu.tripathi@volkswagen.co.in>
2024-08-19 10:18:06 +0530

    adding Model code

--- Test/home/models.py
+++ Test/home/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+
+class Product(models.Model):
+    pass
+
